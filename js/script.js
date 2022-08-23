2f0ae2394b72a4db6427e6ac14a9af52bcb579db
Diana Karime <71475238+KarimeOrtiz@users.noreply.github.com>
2022-08-22 19:53:30 -0700

    style

--- js/script.js
+++ js/script.js
@@ -16,13 +16,45 @@ function toggleImportant(){
     }
 }
 
-function hide(){
-    document.getElementById("#form").className = "hide";
+function saveTask(){
+    console.log("saving task");
+
+    let title = $("#txtTitle").val();
+    let description = $("#txtDescription").val();
+    let dueDate = $("#selDueDate").val();
+    let color = $("#selColor").val();
+    let emoji = $("#selEmoji").val();
+    let location = $("#txtLocation").val();
+    let status = $("#selStatus").val();
+    let notification = $("#chkNotification").prop("checked");
+
+    let task = new Task(isImportant, title, dueDate, description, color, emoji, location, status, notification);
+    displayTask(task);
+}
+
+function displayTask(task){
+    let syntax = `<div id = "newone" style="background-color:${task.color}">
+        <h5>${task.title}</h5>
+        <h5>${task.description}</h5>
+        <h5>${task.dueDate}</h5>
+        <h5>${task.color}</h5>
+        <h5>${task.emoji}</h5>
+        <h5>${task.location}</h5>
+        <h5>${task.status}</h5>
+        <h5>${task.notification}</h5>
+    </div>`;
+
+    $("#pendingTasks").append(syntax);
 }
 
 function init(){
     console.log("Task Manager page!");
+
+    //assigns events
     $("#iImportant").click(toggleImportant);
+    $("#btnSave").click(saveTask);
+
+    //load initial data
 }
 
 window.onload = init;
---
+++ js/test1.js
@@ -0,0 +1,52 @@
+//dog constructor
+function Dog(name, age, color){
+    this.name = name;
+    this.age = age;
+    this.color = color;
+}
+
+//class
+class Cat {
+    constructor(name, age, color){
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
+}
+
+function testObjects(){
+    console.log("Test objects");
+
+    // 1 - Object literal
+    let lola = {
+        name : "Lola",
+        age: 3,
+        color: "Pink"
+    };
+
+    console.log(lola);
+
+    let fido = {
+        Name : "Fido",
+        Age: "4",
+        Color: "Gray",
+        Race: "XYZ"
+    };
+
+    console.log(fido);
+
+    // 2 - Object constructor
+    let dog3 = new Dog("Trex",1,"drakgreen");
+    console.log(dog3)
+
+    let dog4 = new Dog("Trex",1,"drakgreen");
+    console.log(dog4);
+
+    // 3 - Class
+    let cat1 = new Cat("Dr",1,"White");
+    console.log(cat1);
+
+}
+
+//testing
+testObjects();
